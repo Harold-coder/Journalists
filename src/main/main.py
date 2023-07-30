bc1171def748b67e63ac5e61427d679093602d72
Harold Castiaux <harcast@amazon.com>
2023-07-30 15:30:52 -0400

    Connected the db

--- src/main/main.py
+++ src/main/main.py
@@ -1,5 +1,6 @@
 import openai
 import NewYorkTimes
+import mysql.connector
 from ApiKeys import ApiKeys
 openai.organization = ApiKeys.openAiOrg
 openai.api_key = ApiKeys.openAiApiKey
@@ -9,9 +10,25 @@ topStories = NewYorkTimes.NewYorkTimes(type="topStories")
 
 print(topStories.titles)
 
+mydb = mysql.connector.connect(
+  host="www.db4free.net",
+  user="topsecret",
+  password=ApiKeys.databasePassword,
+  database="topsecret"
+)
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO posts (newTitle, newContent, oldTitle, oldContent, url, image) VALUES ('ugdfweyuwdbuydgdwbiyugxbivx', 'ucevjcvuxwvxbiwguyxhvuxvuyxvuxyvx ycguyvcywciwugcicugicgwuyicgigyc gicgviwgcwbiyckvcbiywvcuywcigcbuyicwgc uycvuiykcwgiyckwvcuicwlgwc', 'old', 'new', 'test', 'test')"
+mycursor.execute(sql)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
+
 # system_msg = 'You are talking to a 10 year old asian girl'
 
-# # Define the user message
+# # # Define the user message
 # user_msg = 'Tell her a very short story that teaches her a lesson about life.'
 
 # # Create a dataset using GPT
